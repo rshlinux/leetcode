584d6dd0541e49b99ead065b2038a7e1e6bd0a1c
Ramesh <rshlinux@gmail.com>
2020-02-01 14:11:13 -0800

    Create lc338_counting_bits.c
    Runtime: 32 ms, faster than 97.59%
    Memory Usage: 11.8 MB, less than 100.00%

---
+++ lc338_counting_bits.c
@@ -0,0 +1,30 @@
+/*
+ * Returns the number of bits set
+ */
+int bitcount(int num) {
+    int c = 0;
+    for (c = 0;num;c++) {
+        num = num & (num - 1);
+    }
+    return c;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* countBits(int num, int* returnSize) {
+
+        int i = 0;
+        int *res = 0;
+ 
+        res =  (int *)malloc(sizeof(int) * (num +1));
+        if (!res) {
+            goto done;
+        } 
+        for (i = 0;i <= num; i++) {
+            res[i] = bitcount(i); 
+        }
+done:
+        *returnSize = num + 1;
+        return res;
+}
