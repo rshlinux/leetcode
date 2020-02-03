50a10533d1e53c0da11552c31d32c55953e4cae0
Ramesh <rshlinux@gmail.com>
2020-02-02 21:48:30 -0800

    Create lc100_is_same_tree.cpp
    Runtime: 4 ms, faster than 60.96%.
    Memory Usage: 9.6 MB, less than 100.00%

---
+++ lc100_is_same_tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if ((!p) && (!q)) {
+            return true;
+        } else if ( ((!p) && (q)) || ((!q) && (p)) ) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        }
+        return (isSameTree(p->left, q->left) &&
+            isSameTree(p->right, q->right));
+    }
+};
